50c7f913b18cc2121bfe7a2ddc667d40b621ce34
0xd0a2 <0xd0a2@xviiicen.sc>
2020-12-04 00:25:13 +0300

    update world

--- main.love/world/main.lua
+++ main.love/world/main.lua
@@ -4,8 +4,8 @@ function initWorld()
     local cursor = {
         sx = 0,
         sy = 0,
-        wx = 0,
-        wy = 0 
+        x = 0,
+        y = 0
     }
     -- map size: 50 au (meters)
     local map = {
@@ -13,8 +13,8 @@ function initWorld()
         y = 149597870700 * 50
     }
     local camera = {
-        x = 341,
-        y = 192,
+        x = 0,
+        y = 0,
         zoom = 1,
         angle = 0,
         sx = 0,
@@ -35,45 +35,68 @@ function initWorld()
         interval = 100
     }
     local objects = {
-        planet = {
-            x = 0,
+        sun = {
+            x = 1000000,
+            y = 0,
+            radius = 100000,
+            mass = 6 * 10 ^ 18,
+            angle = 0,
+            vx = 0,
+            vy = 0,
+            acceleration = 0,
+            controlled = false,
+            xs = 0,
+            ys = 0
+        },
+        planet1 = {
+            x = -150,
             y = 0,
             radius = 75,
             mass = 6 * 10 ^ 11,
             angle = 0,
-            fgravity = 0,
-            agravity = 0,
             vx = 0,
-            vy = 0,
+            vy = -20375,
             acceleration = 0,
+            controlled = false,
+            xs = 0,
+            ys = 0
+        },
+        planet2 = {
+            x = 150,
+            y = 0,
+            radius = 75,
+            mass = 6 * 10 ^ 11,
+            angle = 0,
+            vx = 0,
+            vy = -20000,
+            acceleration = 0,
+            controlled = false,
             xs = 0,
             ys = 0
         },    
         sat1 = {
             x = 0,
-            y = -300,
+            y = -1000,
             radius = 5,
             mass = 10,
             angle = 0,
-            fgravity = 0,
-            agravity = 0,
-            vx = 200,
+            vx = 400,
             vy = 0,
             acceleration = 2,
+            controlled = true,
             xs = 0,
             ys = 0
         },
         sat2 = {
             x = 0,
-            y = -350,
+            y = -1200,
             radius = 5,
             mass = 10,
             angle = 0,
-            fgravity = 0,
-            agravity = 0,
-            vx = 180,
+            vx = 400,
             vy = 0,
             acceleration = 2,
+            controlled = 1,
             xs = 0,
             ys = 0
         }
