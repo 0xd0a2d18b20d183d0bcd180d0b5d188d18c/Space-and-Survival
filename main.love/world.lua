6403d80599ca30e1ebf0d6e0ad71a3c75124971b
0xd0a2 <0xd0a2@xviiicen.sc>
2020-12-13 16:34:11 +0300

    update world init

--- main.love/world.lua
+++ main.love/world.lua
@@ -1,5 +1,3 @@
-local json = require "json"
-
 local world = {}
 
 function initWorld()
@@ -7,16 +5,18 @@ function initWorld()
         sx = 0,
         sy = 0,
         x = 0,
-        y = 0
+        y = 0,
+        tempx = 0,
+        tempy = 0
     }
-    -- map size: 50 au (meters)
+    -- map size: 1 au (meters)
     local map = {
-        x = 149597870700 * 50,
-        y = 149597870700 * 50
+        x = 149597870700,
+        y = 149597870700
     }
     local settings = {
-        x = 0,
-        y = 0,
+        x = love.graphics.getWidth() / 2,
+        y = love.graphics.getHeight() / 2,
         zoom = 1,
         angle = 0,
         sx = 0,
@@ -25,41 +25,28 @@ function initWorld()
         sh = love.graphics.getHeight()
     }
     local objects = {
-        sat = {
-            x = 0,
-            y = -400,
-            radius = 60,
-            mass = 10,
-            angle = 0,
-            controlled = 1,
-            vx = 400,
-            vy = 0,
-            va = 0,
-            acceleration = 100,
-            rotation = math.pi,
-            image = love.graphics.newImage("assets/sat.png"),
-            xs = 0,
-            ys = 0
-        },
         moon = {
-            x = 0,
-            y = 0,
+            x = love.graphics.getWidth() / 2,
+            y = love.graphics.getHeight() / 2,
             radius = 210,
-            mass = 6 * 10 ^ 11,
+            mass = 7 * 10 ^ 11,
+            type = "planet",
             angle = 0,
             controlled = 0,
             vx = 0,
             vy = 0,
-            va = 0.2,
+            va = 0.02,
             acceleration = 0,
             rotation = math.pi,
             image = love.graphics.newImage("assets/moon.png"),
             xs = 0,
             ys = 0
-        },
-
+        }
     }
-    return map, settings, objects, cursor
+    local state = {
+        stop = 0
+    }
+    return map, settings, objects, cursor, state
 end
 
 return world
